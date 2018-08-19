dc7824153d6dd7887bc7520755cf2c3b1f9f2924
jinboMa <596756194@qq.com>
2018-08-19 23:46:05 +0800

    feat: init project

---
+++ src/components/Chart.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="chart">
+    图表组件
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class Home extends Vue {}
+</script>
---
+++ src/views/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="index">
+    待创建
+    <chart/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import Chart from '@/components/Chart.vue'
+
+@Component({
+  components: {
+  Chart
+  }
+  })
+export default class Index extends Vue {}
+</script>
